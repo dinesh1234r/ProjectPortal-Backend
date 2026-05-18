76a981613e7520838282f0297203c2510de98abf
DineshKumar <dineshkumar.r8590@gmail.com>
2026-05-18 16:01:08 +0530

    Added TaskItem and DTO

---
+++ Models/TaskItem.cs
@@ -0,0 +1,16 @@
+namespace ProjectPortal.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+    
+    public bool IsCompleted { get; set; }
+    
+    public int AssignedUserId { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
